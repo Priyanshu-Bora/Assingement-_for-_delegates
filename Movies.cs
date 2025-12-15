079fcbe0b177dc9993c194c5cbcfd55de9262008
Priyanshu Bora <135652070+Priyanshu-Bora@users.noreply.github.com>
2025-12-16 00:15:50 +0530

    Create Movies.cs

---
+++ Movies.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie
+{
+    public class Movies
+    {
+        public int Part { get; set; }
+        public string MovieName { get; set; }
+
+        public String MovieDescription { get; set; }
+        public String ActorName { get; set; }
+        public String ReleaseData {  get; set; }
+        public String Language {  get; set; }
+    }
+}
